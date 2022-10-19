80b55c1605e069413674e12d593eaabe786af836
martinblie <martin.liest@gmail.com>
2022-10-19 21:03:15 +0200

    cleanup

---
+++ Project/src/distancesensors.ads
@@ -0,0 +1,10 @@
+with MicroBit.IOsForTasking; use MicroBit.IOsForTasking;
+
+package DistanceSensors is
+
+   function GetDistanceUltrasonic(TriggerPin : Pin_Id; EchoPin : Pin_Id) return Integer;
+   -- HC-SR04 ultrasonic sensor
+   
+  
+
+end DistanceSensors;
--- Project/src/main.adb
+++ Project/src/main.adb
@@ -1,18 +1,29 @@
 with CarObject; use CarObject;
-with MicroBit.IOsForTasking;
+with DistanceSensors; use DistanceSensors;
+with MicroBit.IOsForTasking; use MicroBit.IOsForTasking;
+with MicroBit.DisplayRT;
+with MicroBit.Console;
+with MicroBit.TimeWithRTC1;
 
 procedure Main is
+   Distance : Integer := 0;
+   NewDistance : Integer;
+
 begin
-  loop
 
-
-      Car.SetDirectionLB(False);
-      Car.SetSpeedLeft(150);
-
-      MicroBit.IOsForTasking.Set(14,False);
-  end loop;
-
-
+   loop
+      NewDistance := GetDistanceUltrasonic(0,1);
+      Microbit.TimeWithRTC1.Delay_Ms(500);
+      if NewDistance /= Distance then
+       --  MicroBit.DisplayRT.Clear;
+       --  MicroBit.DisplayRT.Display(NewDistance'Image);
+         Distance := NewDistance;
+         MicroBit.Console.Put_Line(NewDistance'Image);
+         MicroBit.Console.New_Line;
+      end if;
+      MicroBit.Console.Put_Line("xxxxxxxxxxxxxx");
+         MicroBit.Console.New_Line;
+   end loop;
 
 
 
